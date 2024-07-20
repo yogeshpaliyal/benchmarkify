b24519da1a62436f6853c61c4a55a5d19078a250
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2024-07-20 20:26:10 +0530

    feat: fix path and title

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/BenchMarkify/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
